49eef87a8d55fa2b78973bd38387d33414954742
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-06-19 13:38:41 -0300

    Initial commit

---
+++ src/main/java/br/com/infnet/PadraoProjetoSolid/PadraoProjetoSolidApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.PadraoProjetoSolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraoProjetoSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraoProjetoSolidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/infnet/PadraoProjetoSolid/PadraoProjetoSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infnet.PadraoProjetoSolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadraoProjetoSolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
